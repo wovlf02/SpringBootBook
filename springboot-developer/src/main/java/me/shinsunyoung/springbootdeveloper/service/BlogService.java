05c8d618f01efa0092d6e2d4c84cd360ed543d1d
wovlf02 <onlygithub02@gmail.com>
2025-02-16 16:37:26 +0900

    182page

---
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/service/BlogService.java
@@ -0,0 +1,19 @@
+package me.shinsunyoung.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+import me.shinsunyoung.springbootdeveloper.domain.Article;
+import me.shinsunyoung.springbootdeveloper.dto.AddArticleRequest;
+import me.shinsunyoung.springbootdeveloper.repository.BlogRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    // 블로그 글 추가 메서드
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+}
