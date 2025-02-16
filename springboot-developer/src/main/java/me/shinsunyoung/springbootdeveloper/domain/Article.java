719d86398b17f0ee2c76bdc2a1531c06c32f568b
wovlf02 <onlygithub02@gmail.com>
2025-02-17 01:33:49 +0900

    225page complete

--- springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
+++ springboot-developer/src/main/java/me/shinsunyoung/springbootdeveloper/domain/Article.java
@@ -5,10 +5,16 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
 
     @Id
@@ -22,9 +28,23 @@ public class Article {
     @Column(name = "content", nullable = false)
     private String content;
 
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
     @Builder
     public Article(String title, String content) {
         this.title = title;
         this.content = content;
     }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
 }
